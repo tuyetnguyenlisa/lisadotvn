1378fa32540864da32bba5d6c3cf626a80c96d1c
Tuyết Nguyễn <69139334+tuyetnguyenlisa@users.noreply.github.com>
2020-08-23 10:02:20 +0700

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -3,6 +3,7 @@
     <form itemprop="potentialAction" itemscope itemtype="http://schema.org/SearchAction" method="get" class="form-inline" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     	<meta itemprop="target" content="<?php echo home_url();?>?s={s}"/>      
 		<input itemprop="query-input" type="text" class="form-control field" value="<?php echo get_search_query()?>" name="s" id="s" placeholder="<?php esc_attr_e( 'Searching...', 'devvn' ); ?>" />
-		<button type="submit" class="btn btn-default" id="searchsubmit"><?php esc_attr_e( 'Search', 'devvn' ); ?></button>
+		<button type="submit" class="btn btn-default" id="searchsubmit"><?php esc_attr_e( 'Search', 'devvn' ); ?>
+	    </button>
     </form>
 </div>
